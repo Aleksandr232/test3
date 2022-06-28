cb5f3b6cea2d2575a4878b1d66151f117d905d50
Aleksandr232 <aleksrowing232@gmail.com>
2022-06-28 15:42:32 +0300

    arrow

--- src/components/case/case.js
+++ src/components/case/case.js
@@ -1,10 +1,29 @@
 import React from "react";
-import './case.css'
+
+/* import men from './img/men.png'; */
+import ne from './ne.png';
+import rb from './rb.png';
+import at from './at.png';
+import ab from './ab.png';
+import men from './men.png';
+import arrow from './arrow.png'
+
+
+import './case.css';
 
 const Case=()=>{
     return(
         <div className="case">
             <div className="case_title">Кейсы</div>
+            <img className="ne" src={ne} alt="" />
+            <img className="at" src={at} alt="" />
+            <img className="rb" src={rb} alt="" />
+            <img className="ab" src={ab} alt="" />
+            <img className="men"  src={men} alt="" />
+            <div className="elclips">
+                <img className="arrow" src={arrow}></img>
+                <div className="arrow_title">Показать ещё</div>
+            </div>
         </div>
     )
 }
