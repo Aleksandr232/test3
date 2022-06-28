7a1ca979e667d0669d848bf5c4b64acb75ed5ca0
Aleksandr232 <aleksrowing232@gmail.com>
2022-06-28 15:07:24 +0300

    case

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/header/Header";
 import About from "./components/about/about";
+import Case from "./components/case/case";
 
 function App() {
   return (
     <>
     <Header/>
     <About/>
+    <Case/>
     </>
   );
 }
---
+++ src/components/case/case.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './case.css'
+
+const Case=()=>{
+    return(
+        <div className="case">
+            <div className="case_title">Кейсы</div>
+        </div>
+    )
+}
+
+
+export default Case
