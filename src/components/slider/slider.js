431fac8a656cf39335aa09eea7efb6da7a2ed489
Aleksandr232 <aleksrowing232@gmail.com>
2022-07-03 23:53:40 +0300

    slide

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -12,20 +12,26 @@ const handleDragStart = (e) => e.preventDefault();
 
 const items = [
     <div className="slides">
-         <img className="slide1" src={slide1} onDragStart={handleDragStart} role="presentation" />,
-         <img className="slide2"  src={slide1} onDragStart={handleDragStart} role="presentation" />,
-         <img className="slide3" src={slide1} onDragStart={handleDragStart} role="presentation" />,
-         <img className="slide4" src={slide1} onDragStart={handleDragStart} role="presentation" />,
-         <img className="slide5" src={slide1} onDragStart={handleDragStart} role="presentation" />,
-         <img className="slide6" src={polslider} onDragStart={handleDragStart} role="presentation" />,
+         <img className="slide1" src={slide1} onDragStart={handleDragStart} role="presentation" />
+         <div className="textslide">Enter</div>
+         <img className="slide2"  src={slide1} onDragStart={handleDragStart} role="presentation" />
+         <div className="textslide1">Enter</div>
+         <img className="slide3" src={slide1} onDragStart={handleDragStart} role="presentation" />
+         <div className="textslide2">Enter</div>
+         <img className="slide4" src={slide1} onDragStart={handleDragStart} role="presentation" />
+         <div className="textslide3">Enter</div>
+         <img className="slide5" src={slide1} onDragStart={handleDragStart} role="presentation" />
+         <div className="textslide4">Enter</div>
+         <img className="slide6" src={polslider} onDragStart={handleDragStart} role="presentation" />
+         
     </div>,
     <div>
-    <img className="slide0" src={slide1} onDragStart={handleDragStart} role="presentation" />,
-    <img className="slide2" src={slide1} onDragStart={handleDragStart} role="presentation" />,
-    <img className="slide3" src={slide1} onDragStart={handleDragStart} role="presentation" />,
-    <img className="slide4" src={slide1} onDragStart={handleDragStart} role="presentation" />,
-    <img className="slide5" src={slide1} onDragStart={handleDragStart} role="presentation" />,
-    <img className="slide6" src={polslider} onDragStart={handleDragStart} role="presentation" />, 
+    <img className="slide0" src={slide1} onDragStart={handleDragStart} role="presentation" />
+    <img className="slide2" src={slide1} onDragStart={handleDragStart} role="presentation" />
+    <img className="slide3" src={slide1} onDragStart={handleDragStart} role="presentation" />
+    <img className="slide4" src={slide1} onDragStart={handleDragStart} role="presentation" />
+    <img className="slide5" src={slide1} onDragStart={handleDragStart} role="presentation" />
+    <img className="slide6" src={polslider} onDragStart={handleDragStart} role="presentation"/> 
 </div> 
     
 
