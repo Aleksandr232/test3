374c04e94647be41bdc20f9d22bcd1d311af9fe9
Aleksandr232 <aleksrowing232@gmail.com>
2022-07-05 13:20:35 +0300

    client

--- src/components/clien/client.js
+++ src/components/clien/client.js
@@ -1,13 +1,25 @@
 import React from "react";
+import frame34 from './frame34.png'
+import frame35 from './frame35.png'
+import frame36 from './frame36.png'
+import frame37 from './frame37.png'
+
+
 import './client.css'
 
-import frame34 from './frame34.png'
+
+
+
+
 
 
 const Client=()=>{
     return(
         <div className="group" >
             <img className="img_group1" src={frame34} alt="" />
+            <img className="img_group2" src={frame35} alt="" />
+            <img className="img_group3" src={frame36} alt="" />
+            <img className="img_group4" src={frame37} alt="" />
         </div>
     )
 }
